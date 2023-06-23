db6672a670c6fa4de4d00f17dad96b25f8af513a
test4ment <nkaliczkij@yandex.ru>
2023-06-23 17:31:08 +0000

    v2

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -5,24 +5,31 @@ namespace SquareEquationLib;
 public class SquareEquation{
     public static double[] Solve(double a, double b, double c)
     {
-        if(a == 0) throw new System.ArgumentException();
-        if(Math.Abs(a) == double.PositiveInfinity || Math.Abs(b) == double.PositiveInfinity || Math.Abs(c) == double.PositiveInfinity) throw new System.ArgumentException();
+        double eps = Math.Pow(10, -9);
+
+        if(a < eps && -eps < a) throw new System.ArgumentException();
+        if(new[] {a, b, c}.Any(double.IsInfinity)) throw new System.ArgumentException();
         if(Double.IsNaN(a) || Double.IsNaN(b) || Double.IsNaN(c)) throw new System.ArgumentException();
         
-        double eps = Math.Pow(10, -16);
+        
         double D = b*b - 4*a*c;
        
-        if(D > eps){
-			double x1 = -(b + Math.Sign(b) * (Math.Sqrt(D)) / 2);
-            double[] result = {x1, c / x1};
+        if (D <= -eps) 
+        {
+            double[] result = new double[0];
             return result;
         }
-        else if(-eps < D && D <= eps){
-            double[] result = {-b/(2*a)};
+        else if (-eps < D && D < eps)
+        { 
+            double x1 = (-b) / 2*a;
+            double[] result = {x1};
             return result;
         }
-        else{
-            double [] result = new double[0];
+        else
+        {
+            double x1 = -(b + Math.Sign(b) * Math.Sqrt(D)) / 2;
+            double x2 = c / x1;
+            double[] result = {x1, x2};
             return result;
         }
     }
