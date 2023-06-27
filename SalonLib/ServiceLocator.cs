76491798a4c248755d7aaf1108a380ce6d047874
zaryanezrya <ivan@sha.run>
2023-06-27 11:35:16 +0600

    Добавлен сервис 1

--- SalonLib/ServiceLocator.cs
+++ SalonLib/ServiceLocator.cs
@@ -1,5 +1,20 @@
 ﻿namespace SalonLib;
 public class ServiceLocator
 {
+<<<<<<< HEAD
+=======
+    private static IDictionary<string, Func<string>> store;
+
+    static ServiceLocator()
+    {
+        store = new Dictionary<string, Func<string>>();
+        store.Add("Читать стихи", () => "В читальном зале");
+    }
+
+    public static string GetService(string key)
+    {
+        return store[key].Invoke();
+    }
+>>>>>>> d339683 (Добавлен сервис 1)
 
 }
