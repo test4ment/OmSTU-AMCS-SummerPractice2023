67d77b87cb319218fc6ed2424666ad05703f9d49
test4ment <nkaliczkij@yandex.ru>
2023-07-01 18:36:50 +0700

    StepDefinitions fixes

--- Spaceship/Spaceship.cs
+++ Spaceship/Spaceship.cs
@@ -2,9 +2,14 @@
 
 public class Ship
 {
-    private double[] location; 
-    private double[] instantSpeed;
+    private double[]? location; 
+    private double[]? instantSpeed;
     private bool canMove = true;
+    private int? rotationDeg = null;
+    private int? spinSpeed = null;
+    private bool canSpin = true;
+    private int fuel = 0;
+    private int fuelUsage = 0;
 
     public Ship() {}
 
@@ -32,11 +37,70 @@ public class Ship
         return this.canMove;
     }
 
+    public void SetRotation(int degrees){
+        this.rotationDeg = degrees % 360;
+    }
+
+    public int GetRotation(){
+        return (int)this.rotationDeg;
+    }
+
+    public void SetRotatingSpeed(int degPerSpin){
+        this.spinSpeed = degPerSpin;
+        // this.spinSpeed %= 360; // Очень небольшая оптимизация
+    }
+
+    public int GetSpinSpeed(){
+        return (int)this.spinSpeed;
+    }
+
+    public void SetSpinningState(bool ability){
+        this.canSpin = ability;
+    }
+
+    public bool CanSpin(){
+        return this.canSpin;
+    }
+
+    public void SetFuel(int amount){ // Лучше использовать дозаправку (AddFuel)
+        this.fuel = amount;
+    }
+
+    public void AddFuel(int amount){
+        throw new NotImplementedException();
+    }
+
+    public int GetFuel(){
+        return this.fuel;
+    }
+
+    public void SetFuelUsage(int amount){
+        this.fuelUsage = amount;
+    }
+
+    public int GetFuelUsage(){
+        return this.fuelUsage;
+    }
+
+    public void Spin(){
+        if(this.canSpin && this.rotationDeg != null && this.spinSpeed != null){
+            try{
+                this.rotationDeg += this.spinSpeed % 360;
+                this.rotationDeg %= 360;
+            }
+            catch{
+                throw new Exception();
+            }
+        }
+        else throw new Exception();
+    }
+
     public void Move(){
-        if(canMove){
+        if(this.canMove && this.fuel - this.fuelUsage >= 0){
             try{
                 this.location[0] += this.instantSpeed[0];
                 this.location[1] += this.instantSpeed[1];
+                this.fuel -= this.fuelUsage;
             }
             catch{
                 throw new Exception();
--- Specflowproj/Steps/StepDefinitions.cs
+++ Specflowproj/Steps/StepDefinitions.cs
@@ -1,7 +1,7 @@
-using Spaceship;
-using TechTalk.SpecFlow;
-using FluentAssertions;
 using System;
+using Spaceship;
+using FluentAssertions;
+using TechTalk.SpecFlow;
 
 
 namespace Specflowproj.Steps
@@ -38,11 +38,59 @@ namespace Specflowproj.Steps
            this.spaceship.SetMovingState(false);
        }
 
+       [Given(@"космический корабль имеет топливо в объеме (.*) ед")]
+       public void HasFuel(int amount){
+           this.spaceship.SetFuel(amount);
+           if (this.spaceship.GetLoc() == null){
+               this.spaceship.SetLoc(0, 0);
+           }
+           if (this.spaceship.GetSpeed() == null){
+               this.spaceship.SetSpeed(0, 0);
+           }
+       }
+
+       [Given(@"имеет скорость расхода топлива при движении (.*) ед")]
+       public void FuelUsage(int amount){
+           this.spaceship.SetFuelUsage(amount);
+       }
+
+       [Given(@"космический корабль имеет угол наклона (.*) град к оси OX")]
+       public void HasRotation(int degree){
+           this.spaceship.SetRotation(degree);
+       }
+
+       [Given(@"имеет мгновенную угловую скорость (.*) град")]
+       public void HasSpinSeed(int degree){
+           this.spaceship.SetRotatingSpeed(degree);
+       }
+
+       [Given("космический корабль, угол наклона которого невозможно определить")]
+       public void NoRotationGiven(){ }
+
+       [Given("мгновенную угловую скорость невозможно определить")]
+       public void NoSpinSpeedGiven(){ }
+
+       [Given("невозможно изменить уголд наклона к оси OX космического корабля")]
+       public void CannotSpin(){
+           this.spaceship.SetSpinningState(false);
+       }
+
        [When("происходит прямолинейное равномерное движение без деформации")]
        public void InvokeMove()
        {
         try{
            this.spaceship.Move();
+           }
+        catch(Exception e){
+            this.exception = e;
+           }
+       }
+
+       [When("происходит вращение вокруг собственной оси")]
+       public void InvokeSpin()
+       {
+        try{
+           this.spaceship.Spin();
            }
         catch(Exception e){
             this.exception = e;
@@ -59,6 +107,16 @@ namespace Specflowproj.Steps
        public void ResultExcept()
        {
            this.exception.Should().NotBeNull();
+       }
+
+       [Then(@"новый объем топлива космического корабля равен (.*) ед")]
+       public void ResultFuel(int expect){
+           this.spaceship.GetFuel().Should().Be(expect);
+       }
+
+       [Then(@"угол наклона космического корабля к оси OX составляет (.*) град")]
+       public void ResultRotation(int expect){
+           this.spaceship.GetRotation().Should().Be(expect);
        }
     }
 }
